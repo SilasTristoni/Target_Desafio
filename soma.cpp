295e7a9fe77c1f44356d84233e775e791ed6728b
SilasTristoni <silas.tristoni12@gmail.com>
2024-09-01 18:32:24 -0300

    Add files via upload

--- soma.cpp
+++ soma.cpp
@@ -8,7 +8,6 @@ Ao final do processamento, qual será o valor da variável SOMA?*/
 using namespace std;
 int indice = 12, soma = 0, k = 1;
 main(){
-	//condição de soma de k enquanto ele for menor que 13
 	while(k<indice){
 		k = k+1;
 		soma = soma +k;
